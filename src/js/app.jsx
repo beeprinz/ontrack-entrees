5480bc49a548f5ac543328c435a3a70ec7f72a9c
Brendan Prinz <brendanprinz@gmail.com>
2018-01-30 18:04:22 -0800

    All empty containers and components avaible

--- src/js/app.jsx
+++ src/js/app.jsx
@@ -1,10 +1,39 @@
 import React from 'react';
+import { 
+  HashRouter as Router, 
+  Route 
+} from 'react-router-dom';
+import AccountSettings from './containers/AccountSettings/SignUp/SignUp';
+import Cart from './containers/Cart/OrderList/OrderList';
+import PaymentForm from './containers/Cart/PaymentForm/PaymentForm'
+import LandingPage from './containers/LandingPage/LandingNavbar/landingPage';
+import Login from './containers/LandingPage/Login/Login';
+import SearchBar from './containers/LandingPage/SearchBar/SearchBar';
+
+import OrderHistory from './containers/OrderHistory/HistoryList/HistoryList';
+import RestaurantDetail from './containers/RestaurantDetail/MenuItem/MenuItem';
+import RestaurantList from './containers/RestaurantSearch/RestaurantList/RestaurantList';
+import SignUp from './containers/SignUp/SignUpForm/SignUpForm';
+import Error from './components/NotFound/NotFound';
+import ThankYou from './components/ThankYou/ThankYou';
 
 export default class App extends React.Component {
   render() {
     return (
-      <div className='container'>
-      </div>
+      <Router>
+        <div className='container'> 
+          <Route exact path='/' component={ LandingPage } />
+          <Route path='/signup' component={ SignUp } />
+          <Route path='/nearby' component={ RestaurantList } />
+          <Route path='/user/:id' component={ AccountSettings } />
+          <Route path='/user/:id/orders' component={ OrderHistory } />
+          <Route path='/user/:id/cart' component={ Cart } />
+          <Route path='/restaurant/:id' component={ RestaurantList } />
+          <Route path='/thankyou' component={ ThankYou } />
+          <Route path='/NotFound' component={ NotFound } />
+
+        </div>
+      </Router>
     );
   }
 }
--- src/js/rootReducer.js
+++ src/js/rootReducer.js
@@ -1,7 +1,19 @@
 import { combineReducers } from 'redux';
+import signupReducer from './containers/AccountSettings/SignUp/signupReducer';
+import orderlistReducer from './containers/Cart/OrderList/orderlistReducer';
+import paymentformReducer from './containers/Cart/PaymentForm/paymentformReducer';
+import landingpageReducer from './containers/LandingPage/LandingNavBar/landingpageReducer';
+import loginReducer from './containers/LandingPage/Login/loginReducer';
+import searchbarReducer from './containers/LandingPage/SearchBar/searchbarReducers';
+import menuitemReducer from './containers/RestaurantDetail/MenuItem/menuitemReducer';
+import restaurantlistReducer from './containers/RestaurantSearch/RestaurantList/restaurantlistReducer';
+import signupformReducer from './containers/SignUp/SignUpForm/signupformReducer';
+
 
 const rootReducer = combineReducers({
 // add reducers
 });
 
 export default rootReducer;
+
+//make navbar folder!!!
