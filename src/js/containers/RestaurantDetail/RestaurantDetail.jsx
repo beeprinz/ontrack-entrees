c27cca7a51e2b721496e00993406b440c396bed5
Steven Del Rosario <stevendelro@gmail.com>
2018-02-05 08:58:01 -0800

    Typo in super instance.

--- src/js/containers/RestaurantDetail/RestaurantDetail.jsx
+++ src/js/containers/RestaurantDetail/RestaurantDetail.jsx
@@ -1,10 +1,8 @@
 import React, { Component } from "react";
 
-
-
 export default class RestaurantDetail extends React.Component {
   constructor(props) {
-    suoper(props);
+    super(props);
     //initialize state up in this bitch.
   }
   render() {
