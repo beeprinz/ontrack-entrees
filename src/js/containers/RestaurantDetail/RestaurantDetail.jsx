e39a3f44915a2735f6519d84bd376d40d957b992
Steven Del Rosario <stevendelro@gmail.com>
2018-02-04 20:14:50 -0800

    Added RestaurantDetail files to source.

---
+++ src/js/containers/RestaurantDetail/RestaurantDetail.jsx
@@ -0,0 +1,16 @@
+import React, { Component } from "react";
+
+
+
+export default class RestaurantDetail extends React.Component {
+  constructor(props) {
+    suoper(props);
+    //initialize state up in this bitch.
+  }
+  render() {
+    return (
+      <div>
+      </div>
+    );
+  }
+}
